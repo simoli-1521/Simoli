0c4ffe8d2a39ff7429826586e4f8b3baf3664bd4
Ivan212-INH <nabiljapar@gmail.com>
2025-02-13 05:37:04 +0700

    Initial commit

---
+++ app/Filament/Resources/TugasResource.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\TugasResource\Pages;
+use App\Filament\Resources\TugasResource\RelationManagers;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Columns\TextColumn;
+use App\Models\Tugas;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class TugasResource extends Resource
+{
+    protected static ?string $model = Tugas::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('nama_tugas'),
+                TextInput::make('kode_tugas'),
+                TextInput::make('deskripsi_tugas'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('nama_tugas'),
+                TextColumn::make('deskripsi_tugas'),
+                TextColumn::make('kode_tugas'),
+            ])
+            ->emptyStateHeading('Tidak ada tugas')
+            ->emptyStateDescription('Silahkan tambah     tugas terlebih dahulu.')
+
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make()
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTugas::route('/'),
+            'create' => Pages\CreateTugas::route('/create'),
+            'edit' => Pages\EditTugas::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ database/migrations/2025_02_12_091445_create_tugas_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tugas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_tugas');
+            $table->string('deskripsi_tugas');
+            $table->integer('kode_tugas');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tugas');
+    }
+};
