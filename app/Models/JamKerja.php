c6771765489b6ca5b32f580e660b7b722fc858e2
Ivan212-INH <nabiljapar@gmail.com>
2025-03-12 10:58:41 +0700

    edited jam kerja

--- app/Models/JamKerja.php
+++ app/Models/JamKerja.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use App\Models\Surat;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class JamKerja extends Model
 {
+    use HasFactory;
+
     protected $guarded = [];
+
+    protected $table = 'jam_kerjas';
+
+
+    // public function surat()
+    // {
+    //     return $this->belongsTo(Surat::class, 'surat_id', 'id');
+    // }
 }
