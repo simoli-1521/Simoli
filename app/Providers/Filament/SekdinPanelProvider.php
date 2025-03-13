def32b25dd89772d0251271741f96551448720ef
Ivan212-INH <nabiljapar@gmail.com>
2025-03-13 16:15:31 +0700

    temporarily add every page for sekdin panel

--- app/Providers/Filament/SekdinPanelProvider.php
+++ app/Providers/Filament/SekdinPanelProvider.php
@@ -29,8 +29,8 @@ class SekdinPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Amber,
             ])
-            ->discoverResources(in: app_path('Filament/Sekdin/Resources'), for: 'App\\Filament\\Sekdin\\Resources')
-            ->discoverPages(in: app_path('Filament/Sekdin/Pages'), for: 'App\\Filament\\Sekdin\\Pages')
+            ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
+            ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
                 Pages\Dashboard::class,
             ])
