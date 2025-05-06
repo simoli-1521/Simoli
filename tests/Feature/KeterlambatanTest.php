f2f046d237af6c453be439813330fc207d366998
Ivan212-INH <nabiljapar@gmail.com>
2025-05-06 23:20:22 +0700

    add keterlambatan test

--- tests/Feature/KeterlambatanTest.php
+++ tests/Feature/KeterlambatanTest.php
@@ -1,7 +1,21 @@
 <?php
 
-test('example', function () {
-    $response = $this->get('/');
+test('halaman keterlambatan dapat diakses oleh Admin', function () {
+    $response = $this->get('/admin/keterlambatans');
+    $response->assertStatus(302);
+});
 
-    $response->assertStatus(200);
+test('halaman keterlambatan dapat diakses oleh Kepala Dinas', function () {
+    $response = $this->get('/kadin/keterlambatans');
+    $response->assertStatus(302);
+});
+
+test('halaman keterlambatan dapat diakses oleh Sekretaris Dinas', function () {
+    $response = $this->get('/sekdin/keterlambatans');
+    $response->assertStatus(302);
+});
+
+test('halaman keterlambatan dapat diakses oleh Petugas', function () {
+    $response = $this->get('/petugas/keterlambatans');
+    $response->assertStatus(302);
 });
